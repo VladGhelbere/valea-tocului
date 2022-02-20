d3b5fd652a259c49400c2f02b6702e10ebc4a9a7
limiteddata <limiteddata09@gmail.com>
2022-02-20 22:54:00 +0200

    Finished front end

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -7,6 +7,8 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import routes from './common/Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const darkTheme = createTheme({
   palette: {
@@ -27,18 +29,29 @@ function App() {
     });
   }, []);
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Router>
-        <Navbar />
-        <Routes>
-          {
-            routes.map((route, i) => <Route path={route.path} element={route.element} key={i} />)
-          }
-        </Routes>
-        <Footer />
-      </Router>
-    </ThemeProvider>
+    <>
+      <ToastContainer
+        theme="dark"
+        position="top-right"
+        autoClose={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable />
+      <ThemeProvider theme={darkTheme}>
 
+        <Router>
+          <Navbar />
+          <Routes>
+            {
+              routes.map((route, i) => <Route path={route.path} element={route.element} key={i} />)
+            }
+          </Routes>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
